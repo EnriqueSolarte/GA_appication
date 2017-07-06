bbfa796e1a44330bed2472905ce00f28eb611589
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2017-07-06 18:57:18 +0800

    Getting max value and index

--- GA_application/GA_application/Fitness.cs
+++ GA_application/GA_application/Fitness.cs
@@ -1,20 +1,31 @@
 ﻿using System;
-
+using System.Linq;
 namespace GA_application
 {
     public class Fitness
     {
         public double sumatoryFitness { get; }
         public double[] fitnessValue { get; }
-        public int indexFitnessMaximo { get;}
 
         private double constantFitness { set; get; }
 
-        public Fitness(int _constantFitness, int population)
+        public double[] target { set; get; }
+
+        public Fitness(int _constantFitness, int populationSize)
         {
             constantFitness = _constantFitness;
-            fitnessValue = new double[population];
+            fitnessValue = new double[populationSize];
         } 
+
+        private double[] GetMaxFitness()
+        {
+            double value = fitnessValue.Max();
+            double index = fitnessValue.ToList().IndexOf(value);
+
+            double[] result = new double[2] { value, index };
+            return result;
+        }
+        
 
 
     }
