0411b3a886ab4b595f56ab933bbf3674667ef53b
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2017-07-06 13:35:19 +0800

    new fucntion

---
+++ GA_application/GA_application/Fitness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GA_application
+{
+    public class Fitness
+    {
+        public double sumatoryFitness { get; }
+        public double[] fitnessValue { get; }
+
+        public Fitness()
+        {
+
+        } 
+        public double[] Evaluation()
+        {
+            double[] _fitness = new double[3];
+            return _fitness;
+        }
+    }
+}
--- GA_application/GA_application/GeneticAlgorithm.cs
+++ GA_application/GA_application/GeneticAlgorithm.cs
@@ -10,21 +10,45 @@ namespace GA_application
     {
  
         public Features features { get; set; }
+        public Fitness fitness { get; set; }
         public double pCrossover { get; set; }
         public double pMutation { get; set; }
-        public double population { get; set; }
         public double numberGenerations { get; set; }
-        
-        private double[] fitness { get; }
 
+
+        
         public GeneticAlgorithm()
         {
+           
+        }
 
+        private double[] EvaluationFitness()
+        {
+            throw new NotImplementedException();
         }
 
         private void RouletteWheelSelection()
         {
+            Random rand = new Random();
+            for (int i = 0; i < features.populationSize; i++)
+            {
+                double test = rand.NextDouble() * fitness.sumatoryFitness;
+                double partSum = fitness.fitnessValue[0];
+                int j = 1;
+                while (partSum < test)
+                {
+                    partSum = partSum + fitness.fitnessValue[j + 1];
+                    j++;
+                    if (j == features.populationSize)
+                    {
+                        j = 0;
+                    }
+                }
+                features.selectedPopulation[i] = features.population[j];
 
+                features.selectedPopulation[0] = features.maxInPopulation;
+                features.UploadPopulation();   
+            }
         }
 
         private void Crossover()
@@ -36,6 +60,12 @@ namespace GA_application
         {
 
         }
+
+        private void Run()
+        {
+
+        }
+
 
     }
 }
