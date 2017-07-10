ffdc52802498e80abb946ffd88c813dc7a18dead
andresito007 <andres.deivid007@gmail.com>
2017-07-10 14:09:01 +0800

    Class Result finished...

--- GA_application/GA_application/MainWindow.xaml.cs
+++ GA_application/GA_application/MainWindow.xaml.cs
@@ -26,15 +26,13 @@ namespace GA_application
             double[] rangeMeasurement = new double[] { 1, 2, 3, 4, 5};
 
             Function evalTarget = new Function(rangeMeasurement);
-            Features f1, f2;
             double[,] result = evalTarget.Evaluation(new double[] { 12, 12, 10.5, 0.01 });
 
             //Population, rangeFeatures, result, rangeMeasuremet
             GeneticAlgorithm GA_1 = new GeneticAlgorithm(1000, rangeFeatures, result, rangeMeasurement);
             //Generation, Pcrossover, Pmutation
             GA_1.Run(50,0.9,0.01);
-
-
+            
             double[] xVar = new double[] { 1, 2, 3, 4, 5};
             double[] yVar = new double[] {result[0,1], result[1, 1], result[2, 1], result[3, 1], result[4, 1] };
 
@@ -42,8 +40,6 @@ namespace GA_application
             GeneticAlgorithm GA_2 = new GeneticAlgorithm(100, new double[,] { { 0, 15 }, { 5, 15 }, { 0, 1 }, {0, 0.1 } }, xVar, yVar);
 
             GA_2.Run(300,0.9,0.01);
-            
-      
         }
     }
 
---
+++ GA_application/GA_application/Results.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_application
+{
+    public class Results
+    {
+        public double[] maxFitnessGA { get; }
+        public double[] meanFitnessGA { get; }
+        public double[] maxErrorGA { get; }
+        public double[][] bestFeaturesGA { get; }
+        public double[,] evaluationGA { get; }
+
+        public Results(double[] _maxFitnessGA, double[] _meanFitnessGA, double[] _maxErrorGA, double[][] _bestFeaturesGA, double[,] _evaluationGA)
+        {
+            maxFitnessGA = _maxFitnessGA;
+            meanFitnessGA = _meanFitnessGA;
+            maxErrorGA = _maxErrorGA;
+            bestFeaturesGA = _bestFeaturesGA;
+            evaluationGA = _evaluationGA;
+        }
+    }
+}
+
