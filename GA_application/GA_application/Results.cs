4983312c18b48db57255a0cc9c954eaf12d903b0
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2017-07-10 14:36:11 +0800

    Code ready

--- GA_application/GA_application/Results.cs
+++ GA_application/GA_application/Results.cs
@@ -14,13 +14,22 @@ namespace GA_application
         public double[][] bestFeaturesGA { get; }
         public double[,] evaluationGA { get; }
 
-        public Results(double[] _maxFitnessGA, double[] _meanFitnessGA, double[] _maxErrorGA, double[][] _bestFeaturesGA, double[,] _evaluationGA)
+        private Function function;
+
+        public Results(double[] _maxFitnessGA, double[] _meanFitnessGA, double[] _maxErrorGA, double[][] _bestFeaturesGA, double[,] _evaluationGA, double[] _rangeOfMeasurement)
         {
             maxFitnessGA = _maxFitnessGA;
             meanFitnessGA = _meanFitnessGA;
             maxErrorGA = _maxErrorGA;
             bestFeaturesGA = _bestFeaturesGA;
             evaluationGA = _evaluationGA;
+
+            function = new Function(_rangeOfMeasurement);
+        }
+
+        public double[,] Evaluation(double[] _feature)
+        {
+            return function.Evaluation(_feature);
         }
     }
 }
